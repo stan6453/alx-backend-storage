8446d976a851f136266d738b696666e8c0e08663
stanley <stanley6453@gmail.com>
2023-07-12 20:55:56 +0100

    created file 0x00-MySQL_Advanced/4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,6 +2,5 @@
 
 CREATE TRIGGER decreases_item
 AFTER INSERT ON orders FOR EACH ROW
-  UPDATE items SET quantity = ((SELECT items.quantity from items where items.name = NEW.item_name) - NEW.number)
-  WHERE NEW.item_name = items.name
-  
+UPDATE items SET quantity = ((SELECT items.quantity from items where items.name = NEW.item_name) - NEW.number)
+WHERE NEW.item_name = items.name
