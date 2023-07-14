deda16e22c5df72842e4ee805e3e7b3b06296c0a
stanley <stanley6453@gmail.com>
2023-07-14 01:14:21 +0100

    created file 0x02-python_async_comprehension/101-average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,5 +1,7 @@
 -- Write a SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students.
 
+DROP procedure IF EXISTS ComputeAverageWeightedScoreForUsers;
+
 DELIMITER //
 
 CREATE PROCEDURE
@@ -8,6 +10,15 @@ BEGIN
   DECLARE weight_factor INT;
 
   SELECT SUM(weight) INTO weight_factor FROM projects;
+
+  UPDATE users INNER JOIN corrections
+  ON users.id = corrections.user_id
+  SET average_score = (
+    SELECT SUM(score * ((SELECT weight FROM projects WHERE projects.id = corrections.project_id)/weight_factor))
+    FROM corrections
+    WHERE corrections.user_id = users.id
+  )
+  WHERE corrections.user_id = users.id;
 
 END //
 
