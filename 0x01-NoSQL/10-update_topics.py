d9f60543d6b1b65184c8585dcc5afadc00987599
stanley <stanley6453@gmail.com>
2023-07-17 20:39:44 +0100

    Improved code style in the codebase

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Write a Python function that changes all topics of a school document based on the name:
+Write a Python function that changes all topics of a school
+document based on the name:
 * Prototype: def update_topics(mongo_collection, name, topics):
 * mongo_collection will be the pymongo collection object
 * name (string) will be the school name to update
