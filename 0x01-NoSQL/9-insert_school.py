60e8b448e498b724a7d5a2492818816ebed25fad
stanley <stanley6453@gmail.com>
2023-07-17 21:21:58 +0100

    modified 0x01-NoSQL/9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -10,4 +10,4 @@ collection based on kwargs:
 
 def insert_school(mongo_collection, **kwargs):
     """ inserts a new document in a collection based on kwargs """
-    mongo_collection.insert_one(kwargs)
+    return mongo_collection.insert_one(kwargs).inserted_id
