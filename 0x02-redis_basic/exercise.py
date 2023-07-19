5a51f8b28b230e2f01c555d145659b905f07507d
stanley <stanley6453@gmail.com>
2023-07-19 18:37:52 +0100

    added the ablility to get data from cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Writing strings to Redis 
+Writing strings to Redis
 """
 import sys
 import redis
@@ -15,7 +15,7 @@ class Cache():
 
     def __init__(self) -> None:
         """
-        Initialize Redis db connection 
+        Initialize Redis db connection
         """
         self._redis = redis.Redis(host='127.0.0.1')
         self._redis.flushdb()
@@ -28,19 +28,21 @@ class Cache():
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable[[Any], Any]]):
+    def get(
+        self,
+        key: str,
+        fn: Optional[Callable[[bytes], Any]] = None
+    ) -> Union[str, bytes, int, float, None]:
         """
         retrieves key-value pair in redis
         """
-        result = self._redis.get(key)
-        if fn is None:
-            return result
-        return fn(result)
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
 
-    def get_int(self: bytes) -> int:
-        """convert to int"""
-        return int.from_bytes(self, sys.byteorder)
+    def get_str(self, key: str) -> Union[str, None]:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
 
-    def get_str(self: bytes) -> str:
-        """convert to string"""
-        return self.decode("utf-8")
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, fn=lambda d: int(d))
