b519f12dd923a0df77bf482523447877867ce832
stanley <stanley6453@gmail.com>
2023-07-20 00:31:09 +0100

    created file 0x02-redis_basic/web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
-"""
-Redis as a http request rate counter
-"""
-import requests
-import redis
-import functools
-from typing import Callable
+"""In this tasks, we will implement a get_page function
+(prototype: def get_page(url: str) -> str:). The core of
+the function is very simple. It uses the requests module
+to obtain the HTML content of a particular URL and returns it.
 
-r = redis.Redis(host='127.0.0.1')
+Start in a new file named web.py and do not reuse the code
+written in exercise.py.
+
+Inside get_page track how many times a particular URL was
+accessed in the key "count:{url}" and cache the result with
+an expiration time of 10 seconds.
+
+Tip: Use http://slowwly.robertomurray.co.uk to simulate
+a slow response and test your caching."""
+
+
+import redis
+import requests
+from functools import wraps
+
+r = redis.Redis()
 
 
 def url_access_count(method):
     """decorator for get_page function"""
-    @functools.wraps(method)
+    @wraps(method)
     def wrapper(url):
         """wrapper function"""
         key = "cached:" + url
@@ -33,7 +45,10 @@ def url_access_count(method):
 
 @url_access_count
 def get_page(url: str) -> str:
-    """
-    make a get request and return the value
-    """
-    return requests.get(url).text
+    """obtain the HTML content of a particular"""
+    results = requests.get(url)
+    return results.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
