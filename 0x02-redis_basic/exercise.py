0326242a8a6c40285d73a944d6b2b090c480a8a3
stanley <stanley6453@gmail.com>
2023-07-19 12:32:07 +0100

    created folder 0x02-redis_basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,22 +16,22 @@ class Cache():
         """
         Initialize Redis db connection 
         """
-        self._redis = redis.Redis()
-        self._redis.flushdb()
+        self.__redis = redis.Redis()
+        self.__redis.flushdb()
 
     def store(self, data: str | bytes | int | float) -> str:
         """
         Store key-value pair in redis
         """
         key = str(uuid.uuid4())
-        self._redis.set(key, data)
+        self.__redis.set(key, data)
         return key
 
     def get(self, key: str, fn: Optional[Callable[[Any], Any]]):
         """
         retrieves key-value pair in redis
         """
-        result = self._redis.get(key)
+        result = self.__redis.get(key)
         if fn is None:
             return result
         return fn(result)
