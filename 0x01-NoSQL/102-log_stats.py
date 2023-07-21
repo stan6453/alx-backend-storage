0adcb64c789761bdd2719970ce75fd64dbcdc57f
Stanley Ezechukwu <stanley6453@gmail.com>
2023-07-21 23:16:02 +0100

    used mongodb aggregation pipeline to calculate stats for nginx logs

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+Write a Python script that provides some stats about Nginx
+logs stored in MongoDB:
+ * Database: logs
+ * Collection: nginx
+ * Display (same as the example):
+   - first line: x logs where x is the number of documents in this collection
+   - second line: Methods:
+   - 5 lines with the number of documents with the
+     method = ["GET", "POST", "PUT", "PATCH", "DELETE"] in this
+     order (see example below - warning: it’s a tabulation before each line)
+   - one line with the number of documents with:
+     + method=GET
+     + path=/status
+"""
+from pymongo import MongoClient
+
+
+def log_stats(nginx):
+    """
+    function that provides some stats about Nginx logs stored in MongoDB:
+    """
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    doc_num = nginx.count_documents({})
+    status_check = nginx.count_documents({"path": "/status", "method": "GET"})
+
+    print("{} logs".format(doc_num))
+    print("Methods:")
+    for item in method:
+        count = nginx.count_documents({"method": item})
+        print("\tmethod {}: {}".format(item, count))
+    print("{} status check".format(status_check))
+
+    most_present_ips = nginx.aggregate([
+        {
+            "$group": {"_id": "$ip", "count": {"$sum": 1}}
+        },
+        {
+            "$sort": {"count": -1, "ip": -1}
+        },
+        {
+            "$limit": 10
+        }
+    ])
+
+    print("IPs:")
+    for line in most_present_ips:
+      print("\t{}: {}".format(line.get("_id"), line.get("count")))
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    log_stats(client.logs.nginx)
