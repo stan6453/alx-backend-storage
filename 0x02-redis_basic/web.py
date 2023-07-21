991a4ca30de3d4c19f2567259596d7d1dc083521
stanley <stanley6453@gmail.com>
2023-07-22 00:15:46 +0100

    implemented rate counting for url in 0x02-redis_basic/web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -29,7 +29,7 @@ def count_requests(method: Callable) -> Callable:
         #     return redis_db.get(cache_key).decode("utf-8")
 
         result = method(*args, **kwargs)
-        redis_db.setex(cache_key, 10, result)
+        redis_db.setex(cache_key, 9, result)
         return result
 
     return wrapper
